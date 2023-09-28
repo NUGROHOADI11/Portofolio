c7182c8c99d0ed641b2d901f15570f63b5fb4d46
NUGROHOADI11 <deeforce112002@gmail.com>
2023-09-28 23:29:55 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector('.navigasi-nav');
+
+document.querySelector('#menu').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function(e){
+    if(!menu.contains(e.target) && !navbar.contains(e.target)) {
+        navbar.classList.remove('active');
+    }
+})
